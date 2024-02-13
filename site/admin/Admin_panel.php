ada2168866a84392e88ade84603663d63f1566c8
ShHawk <alexandreuzan9@gmail.com>
2024-02-13 22:13:10 +0100

    ADD Admin_panel.php

---
+++ site/admin/Admin_panel.php
@@ -0,0 +1,57 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Tableau de Bord - Espace d'Administration</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="admin-container">
+        <div id="sidebar-menu">
+            <ul>
+                <li><a href="gestion_stocks.php">Gestion des Stocks</a></li>
+                <li><a href="gestion_services.php">Gestion des Services</a></li>
+                <li><a href="gestion_benevoles.php">Gestion des Bénévoles</a></li>
+                <li><a href="gestion_activites.php">Gestion des Activités</a></li>
+                <li><a href="gestion_tickets.php">Gestion des Tickets</a></li>
+            </ul>
+        </div>
+        
+        <div id="main-content">
+            <h1>Tableau de Bord</h1>
+            <div class="dashboard">
+                <div class="dashboard-item">
+                    <h2>Statistiques Rapides</h2>
+                    <!-- Ici, insérez des statistiques comme le nombre de bénévoles, le nombre d'événements à venir, etc. -->
+                </div>
+                <div class="dashboard-item">
+                    <h2>Notifications Récentes</h2>
+                    <!-- z les dernières notifications ou alertes -->
+                </div>
+                <div class="dashboard-item">
+                    <h2>Activités Récentes</h2>
+                    <!-- Un aperçu des activités ou des tâches récentes -->
+                </div>
+                 <!-- Section Ticketing -->
+                 <div class="dashboard-item">
+                    <h2>Tickets Récent</h2>
+                    <!-- Affichez un aperçu des derniers tickets -->
+                </div>
+                <div class="dashboard-item">
+                    <h2>Calendrier des Événements</h2>
+                    <!-- Calendrier ou liste des prochains événements -->
+                </div>
+                <div class="dashboard-item">
+                    <h2>Rappels Importants</h2>
+                    <!-- Liste des rappels ou des tâches urgentes -->
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

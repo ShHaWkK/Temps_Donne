a226c8b11ca0cd793668da08783fa97b64a74335
ShHawk <alexandreuzan9@gmail.com>
2024-02-14 19:57:50 +0100

    ADD Alter table service o  .sql

--- site/Au_temps_Donné.sql
+++ site/Au_temps_Donné.sql
@@ -55,6 +55,13 @@ CREATE TABLE Services (
     ID_Administrateur INT,
     FOREIGN KEY (ID_Administrateur) REFERENCES Utilisateurs(ID_Utilisateur)
 );
+
+ALTER TABLE Services ADD NFC_Tag_Data TEXT;
+ALTER TABLE Services ADD Type_Service VARCHAR(255);
+ALTER TABLE Services ADD Date_Debut DATE;
+ALTER Table Services ADD Date_Fin DATE;
+
+
 CREATE TABLE Formations (
     ID_Formation INT AUTO_INCREMENT PRIMARY KEY,
     Titre VARCHAR(255),

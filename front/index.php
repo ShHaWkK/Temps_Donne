03c0b8793ad62b4b22c6b73a125af338816dd64c
ShHaWkK <alexandreuzan9@gmail.com>
2024-04-04 14:32:13 +0200

    ADD index.php and folder images

---
+++ front/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<title>Devenez bénévole</title>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<?php include 'header/header.php'; ?>
+<body>
+  
+
+<div class="carousel-container">
+  <button class="carousel-arrow">&lt;</button>
+  <div class="carousel-caption">
+    <h1>ENSEMBLE, CHANGEONS DES VIES</h1>
+    <p>Devenez bénévole dès maintenant !</p>
+    <button>JE DEVIENS BÉNÉVOLE</button>
+  </div>
+  <button class="carousel-arrow">&gt;</button>
+</div>
+
+<div class="section">
+  <div class="section-title">QUI SOMMES NOUS ?</div>
+  <p class="section-content">Fondée en 1987 à Saint-Quentin, dans l'Aisne, en réponse à la crise industrielle locale, “Au temps donné” n’a cessé de proposer son aide à ceux qui en ont besoin.
+À l'origine centrée sur la distribution alimentaire et les maraudes urbaines,nous avons progressivement élargi notre gamme de services au fil du temps:
+ de l'administratif aux navettes pour les rendez-vous éloignés, 
+ des cours d'alphabétisation au soutien scolaire, 
+ ainsi que des événements de collecte de fonds et des activités pour les personnes âgées. 
+Notre engagement envers la communauté reste notre priorité.</p>
+  <button class="section-button">Découvrir nos services</button>
+</div>
+
+<div class="location-section">
+   <i class="fas fa-map-marker-alt"></i>
+  <div class="location-text">OÙ NOUS TROUVER ? Le local historique de l'association a été créé à Saint-Quentin...</div>
+</div>
+
+</body>
+</html>

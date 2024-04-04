24e58e461ec8d922d1a47f516af6c998ffc81cfc
ShHaWkK <alexandreuzan9@gmail.com>
2024-04-04 21:18:15 +0200

    Update index.php

--- front/index.php
+++ front/index.php
@@ -8,10 +8,9 @@
 <title>Devenez bénévole</title>
 <link rel="stylesheet" href="css/main.css">
 </head>
-<?php include 'header/header.php'; ?>
+<?php include 'includes/header.php'; ?>
 <body>
   
-
 <div class="carousel-container">
   <button class="carousel-arrow">&lt;</button>
   <div class="carousel-caption">
@@ -25,18 +24,23 @@
 <div class="section">
   <div class="section-title">QUI SOMMES NOUS ?</div>
   <p class="section-content">Fondée en 1987 à Saint-Quentin, dans l'Aisne, en réponse à la crise industrielle locale, “Au temps donné” n’a cessé de proposer son aide à ceux qui en ont besoin.
-À l'origine centrée sur la distribution alimentaire et les maraudes urbaines,nous avons progressivement élargi notre gamme de services au fil du temps:
- de l'administratif aux navettes pour les rendez-vous éloignés, 
- des cours d'alphabétisation au soutien scolaire, 
- ainsi que des événements de collecte de fonds et des activités pour les personnes âgées. 
+  À l'origine centrée sur la distribution alimentaire et les maraudes urbaines,nous avons progressivement élargi notre gamme de services au fil du temps:
+  de l'administratif aux navettes pour les rendez-vous éloignés, des cours d'alphabétisation au soutien scolaire, 
+  ainsi que des événements de collecte de fonds et des activités pour les personnes âgées. 
 Notre engagement envers la communauté reste notre priorité.</p>
-  <button class="section-button">Découvrir nos services</button>
+  <button class="section-button"><i class="fa-solid fa-house"></i> Découvrir nos services</button>
 </div>
 
 <div class="location-section">
-   <i class="fas fa-map-marker-alt"></i>
-  <div class="location-text">OÙ NOUS TROUVER ? Le local historique de l'association a été créé à Saint-Quentin...</div>
+  <h2 class="location-title">OÙ NOUS TROUVER ?</h2>
+  <p class="location-text">Le local historique de l’association a été créé à Saint-Quentin, au 6 boulevard Gambetta...
+    Mais au fil des années, l’association s’est dotée d’un entrepôt et de plusieurs annexes.
+    Que vous souhaitiez vous engager en tant que bénévole ou bénéficier de nos services,
+    découvrez nos différents sites une différence dans la vie des autres.
+  </p>
+  <i class="fas fa-map-marker-alt location-icon"></i>
 </div>
+
 
 </body>
 </html>

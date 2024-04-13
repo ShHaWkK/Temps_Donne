5b759795eeafa12d1d9501921d7639e75d9eeb56
ShHaWkK <alexandreuzan9@gmail.com>
2024-04-13 18:14:39 +0200

    ADD lang page_connexion FR and EN

--- front/inscription_conn/connexion_beneficiaire.php
+++ front/inscription_conn/connexion_beneficiaire.php
@@ -1,38 +1,37 @@
+<?php
+include_once('../includes/lang.php');
+include_once('../includes/head.php');
+?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="<?php echo htmlspecialchars($userLanguage); ?>">
 <head>
   <link rel="stylesheet" href="../css/connexion.css">
+  <title><?php echo htmlspecialchars($data["BENEFICIARY_SPACE_TITLE"]); ?> - Au temps donné</title>
 </head>
-
-<?php
-include_once('../includes/head.php');
-
-echo "<title>Connexion Bénéficiaire - Au temps donné</title>";
-?>
 
 <body>
   <div class="container">
     <div class="left-side">
-      <img src="../images/logo.png" alt="Logo des Bénéficiaires" class="logo">
-      <h1 class="login-title">ESPACE BÉNÉFICIAIRE</h1>
-      <p class="login-subtitle">Votre engagement, notre force</p>
+      <img src="../images/logo.png" alt="Logo" class="logo">
+      <h1 class="login-title"><?php echo htmlspecialchars($data["BENEFICIARY_SPACE_TITLE"]); ?></h1>
+      <p class="login-subtitle"><?php echo htmlspecialchars($data["BENEFICIARY_SPACE_SUBTITLE"]); ?></p>
       <div class="register">
-        <span>Nouveau ici ?</span>
-        <button class="register-button">Créer un compte</button>
+        <span><?php echo htmlspecialchars($data["NEW_HERE"]); ?></span>
+        <button class="register-button"><?php echo htmlspecialchars($data["CREATE_ACCOUNT"]); ?></button>
       </div>
     </div>
     <div class="right-side">
       <form class="login-form">
         <div class="input-group">
-          <label for="email">E-mail:</label>
+          <label for="email"><?php echo htmlspecialchars($data["EMAIL_LABEL"]); ?></label>
           <input type="email" id="email" name="email" required>
         </div>
         <div class="input-group">
-          <label for="password">Mot de passe:</label>
+          <label for="password"><?php echo htmlspecialchars($data["PASSWORD_LABEL"]); ?></label>
           <input type="password" id="password" name="password" required>
         </div>
-        <button type="submit" class="login-button">S'identifier</button>
-        <a href="#" class="forgot-password">Mot de passe oublié ?</a>
+        <button type="submit" class="login-button"><?php echo htmlspecialchars($data["LOGIN_BUTTON"]); ?></button>
+        <a href="#" class="forgot-password"><?php echo htmlspecialchars($data["FORGOT_PASSWORD"]); ?></a>
       </form>
     </div>
   </div>

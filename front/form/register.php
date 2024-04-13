6fb9ae9f94e048c7d6494b7e7b84e9fbd6b0f7ed
Rachid <djouaherrachid@gmail.com>
2024-04-14 00:20:56 +0200

    Add multilinguism for register form

--- front/form/register.php
+++ front/form/register.php
@@ -17,46 +17,46 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
 
 <div class="form-container">
     <div class="form-content">
-        <h1> Je deviens Bénévole </h1>
-        <p>Le bénévolat, comme la solidarité, peuvent prendre diverses formes !
-            Ce formulaire aidera notre équipe de bénévoles à vous proposer des missions faites pour vous.
-        <h2> Informations personnelles </h2>
-        <h3> Genre: * </h3>
+        <h1><?php echo htmlspecialchars($data["I_BECOME_VOLUNTEER"] ?? 'I become volunteer');?></h1>
+        <p><?php echo htmlspecialchars($data["FORM_INTRO_VOLUNTEER"]);?>
+        </p>
+        <h2> <?php echo htmlspecialchars($data["PERSONAL_INFORMATIONS"]);?> </h2>
+        <h3> <?php echo htmlspecialchars($data["GENDER"]);?>: * </h3>
         <div class="line">
-            <label class="radio-label"> Homme <input type="radio" id="genre" name="genre" value="homme" required> </label>
-            <label class="radio-label"> Femme <input type="radio" id="genre" name="genre" value="femme" required> </label>
-            <label class="radio-label"> Autre <input type="radio" id="genre" name="genre" value="autre" required> </label>
+            <label class="radio-label"> <?php echo htmlspecialchars($data["MALE"]);?> <input type="radio" id="genre" name="genre" value="homme" required> </label>
+            <label class="radio-label"> <?php echo htmlspecialchars($data["FEMALE"]);?> <input type="radio" id="genre" name="genre" value="femme" required> </label>
+            <label class="radio-label"> <?php echo htmlspecialchars($data["OTHER"]);?> <input type="radio" id="genre" name="genre" value="autre" required> </label>
         </div>
 
         <div class="line">
             <div class="col">
-                <label for="nom"> <h3>Nom: *</h3></label>
+                <label for="nom"> <h3><?php echo htmlspecialchars($data["SURNAME"]);?>: *</h3></label>
                 <input type="text" id="nom" name="nom" required>
             </div>
             <div class="col">
-                <label for="prenom"> <h3>Prénom: *</h3></label>
+                <label for="prenom"> <h3><?php echo htmlspecialchars($data["NAME"]);?>: *</h3></label>
                 <input type="text" id="prenom" name="prenom" required>
             </div>
         </div>
 
-        <label for="date_naissance"><h3> Date de naissance: * </h3></label>
+        <label for="date_naissance"><h3><?php echo htmlspecialchars($data["BIRTHDAY"]);?>: * </h3></label>
         <input type="date" id="date_naissance" name="date_naissance" required>
 
-        <label for="email"> <h3> Adresse mail: * </h3> </label>
+        <label for="email"> <h3> <?php echo htmlspecialchars($data["EMAIL_LABEL"]);?>: * </h3> </label>
         <input type="email" id="email" name="email" required>
 
         <!-- TODO vérifier si l'adresse mail existe-->
         <?php
         ?>
 
-        <label for="telephone"><h3> Numéro de téléphone: * </h3></label>
+        <label for="telephone"><h3> <?php echo htmlspecialchars($data["PHONE_NUMBER"]);?>: * </h3></label>
         <input type="tel" id="telephone" name="telephone" required>
         <small class="format-info" color="red" >Format: 0123456789</small>
         </fieldset>
 
         <div class="line">
             <div class="col">
-                <label for="nationalite"><h3> Nationalité: * </h3> </label>
+                <label for="nationalite"><h3> <?php echo htmlspecialchars($data["NATIONALITY"]);?>: * </h3> </label>
                 <select id="nationalite" name="nationalite" required>
                     <option value="AF">Afghanistan</option>
                     <option value="ZA">Afrique du Sud</option>
@@ -307,7 +307,7 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
 
             </div>
             <div class="col">
-                <label for="langues"> <h3>Langues: *</h3></label>
+                <label for="langues"> <h3><?php echo htmlspecialchars($data["LANGUAGES"]);?>: *</h3></label>
                 <select id="langues" name="langues[]" multiple >
                     <option value="francais">Francais</option>
                     <option value="anglais">Anglais</option>
@@ -319,31 +319,31 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
         <h3> Adresse: * </h3>
         <textarea id="name" name="name" rows="1" cols="1"></textarea>
 
-        <label for="situation"> <h3> Situation personnelle: *</h3> </label>
+        <label for="situation"> <h3> <?php echo htmlspecialchars($data["PERSONAL_SITUATION"]);?>: *</h3> </label>
         <select id="situation" name="situation" required>
-            <option value="etudiant">Étudiant</option>
-            <option value="employe">Employé</option>
-            <option value="chomeur">Chômeur</option>
-            <option value="retraite">Retraité</option>
+            <option value="etudiant"><?php echo htmlspecialchars($data["STUDENT"]);?></option>
+            <option value="employe"><?php echo htmlspecialchars($data["EMPLOYED"]);?></option>
+            <option value="chomeur"><?php echo htmlspecialchars($data["UNEMPLOYED"]);?></option>
+            <option value="retraite"><?php echo htmlspecialchars($data["RETIRED"]);?></option>
             <!-- Ajoutez d'autres options de situation personnelle ici -->
         </select>
 
-        <h2> Disponibilités et domaine d'intervention </h2>
-        <h3>Mobilité: *</h3>
+        <h2> <?php echo htmlspecialchars($data["AVAILABILITY_INTERVENTION_AREA"]);?> </h2>
+        <h3><?php echo htmlspecialchars($data["MOBILITY"]);?>: *</h3>
         <div class="col">
             <div class="line permis">
                 <label class="switch">
                     <input type="checkbox">
                     <span class="slider round"></span>
                 </label>
-                <a>Permis B</a>
+                <a><?php echo htmlspecialchars($data["DRIVER_LICENSE"]);?></a>
             </div>
             <div class="line permis">
                 <label class="switch">
                     <input type="checkbox">
                     <span class="slider round"></span>
                 </label>
-                <a>Permis Poids Lourd</a>
+                <a><?php echo htmlspecialchars($data["HEAVY_LICENSE"]);?></a>
             </div>
             <div class="line permis">
                 <label class="switch">
@@ -353,62 +353,62 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
                 <a>CACES</a>
             </div>
         </div>
-        <h3>Justificatif: *</h3>
+        <h3><?php echo htmlspecialchars($data["SUPPORTING_DOCUMENTS"]);?>: *</h3>
         <form action="upload.php" method="post" enctype="multipart/form-data" required>
-            <label for="pdfFile">Veuillez envoyer une copie scannée de votre permis de conduire : <br></label>
+            <label for="pdfFile"><?php echo htmlspecialchars($data["SEND_LICENSE"]);?> : <br></label>
             <input type="file" id="pdfFile" name="pdfFile" accept=".pdf">
         </form>
 
-        <h3>Expérience: </h3>
+        <h3><?php echo htmlspecialchars($data["EXPERIENCE"]);?>: </h3>
         <form action="upload.php" method="post" enctype="multipart/form-data" required>
-            <label for="pdfFile"> Veuillez envoyer votre CV : <br></label>
+            <label for="pdfFile"> <?php echo htmlspecialchars($data["SEND_RESUME"]);?> : <br></label>
             <input type="file" id="pdfFile" name="pdfFile" accept=".pdf">
         </form>
 
-        <h3>Disponibilités: </h3>
+        <h3><?php echo htmlspecialchars($data["AVAILABILITY"]);?>: </h3>
         <div class="line">
-            <label class="radio-label"> Disponibilité régulière <input type="radio" name="disponibilite" value="reguliere" required> </label>
-            <label class="radio-label"> Disponibilité ponctuelle <input type="radio" name="disponibilite" value="ponctuelle" required> </label>
+            <label class="radio-label"> <?php echo htmlspecialchars($data["REGULAR_AVAILABILITY"]);?> <input type="radio" name="disponibilite" value="reguliere" required> </label>
+            <label class="radio-label"> <?php echo htmlspecialchars($data["PUNCTUAL_AVAILABILITY"]);?> <input type="radio" name="disponibilite" value="ponctuelle" required> </label>
         </div>
 
         <div class="line">
-            <h4>Je peux consacrer </h4>
+            <h4><?php echo htmlspecialchars($data["I_CAN_DEVOTE"]);?> : </h4>
             <select class="heures" id="heures" name="heures" required>
-                <option value="1">1 demi-journée</option>
-                <option value="2">2 demi-journées</option>
-                <option value="3">3 demi-journées</option>
-                <option value="4">4 demi-journées</option>
-                <option value="5">5 demi-journées</option>
-                <option value="6">6 demi-journées</option>
-                <option value="7">7 demi-journées</option>
-                <option value="8">8 demi-journées</option>
-                <option value="9">9 demi-journées</option>
-                <option value="10">10 demi-journées</option>
-                <option value="11">11 demi-journées</option>
-                <option value="12">12 demi-journées</option>
-                <option value="13">13 demi-journées</option>
-                <option value="14">14 demi-journées</option>
+                <option value="1">1 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="2">2 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="3">3 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="4">4 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="5">5 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="6">6 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="7">7 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="8">8 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="9">9 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="10">10 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="11">11 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="12">12 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="13">13 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
+                <option value="14">14 <?php echo htmlspecialchars($data["HALF_DAY"]);?></option>
             </select>
-            <h4> par semaine pour des missions de bénévolat, le : <br> </h4>
+            <h4> <?php echo htmlspecialchars($data["PER_WEEK_VOLUNTEER_MISSIONS"]);?> <br> </h4>
         </div>
 
         <div class="line">
             <div class="col">
-                <label><input type="checkbox" id="lundi" name="jour" value="lundi"> Lundi</label><br>
-                <label><input type="checkbox" id="mardi" name="jour" value="mardi"> Mardi</label><br>
-                <label><input type="checkbox" id="mercredi" name="jour" value="mercredi"> Mercredi</label><br>
-                <label><input type="checkbox" id="jeudi" name="jour" value="jeudi"> Jeudi</label><br>
+                <label><input type="checkbox" id="lundi" name="jour" value="lundi"> <?php echo htmlspecialchars($data["MONDAY"]);?></label><br>
+                <label><input type="checkbox" id="mardi" name="jour" value="mardi"> <?php echo htmlspecialchars($data["TUESDAY"]);?></label><br>
+                <label><input type="checkbox" id="mercredi" name="jour" value="mercredi"> <?php echo htmlspecialchars($data["WEDNESDAY"]);?></label><br>
+                <label><input type="checkbox" id="jeudi" name="jour" value="jeudi"> <?php echo htmlspecialchars($data["THURSDAY"]);?></label><br>
             </div>
             <div class="col">
-                <label><input type="checkbox" id="vendredi" name="jour" value="vendredi"> Vendredi</label><br>
-                <label><input type="checkbox" id="samedi" name="jour" value="samedi"> Samedi</label><br>
-                <label><input type="checkbox" id="dimanche" name="jour" value="dimanche"> Dimanche</label><br>
+                <label><input type="checkbox" id="vendredi" name="jour" value="vendredi"> <?php echo htmlspecialchars($data["FRIDAY"]);?></label><br>
+                <label><input type="checkbox" id="samedi" name="jour" value="samedi"> <?php echo htmlspecialchars($data["SATURDAY"]);?></label><br>
+                <label><input type="checkbox" id="dimanche" name="jour" value="dimanche"> <?php echo htmlspecialchars($data["SUNDAY"]);?></label><br>
             </div>
         </div>
 
-        <h2>Termes et mentions légales </h2>
+        <h2> <?php echo htmlspecialchars($data["TERMS_LEGAL_NOTICES"]);?> </h2>
         <label>
-            J’ai lu et j'accepte les <a href="#" id="termsLink"> termes et mentions légales *</a>
+            <?php echo htmlspecialchars($data["READ_ACCEPT"]);?> <a href="#" id="termsLink"> <?php echo htmlspecialchars($data["TERMS_LEGAL_NOTICES"]);?> *</a>
             <input type="checkbox" id="légales" name="conditions" value="légales" required>
         </label>
 
@@ -417,10 +417,8 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
                 <span class="close">&times;</span>
                 <!-- Contenu des termes et conditions -->
                 <?php
-                // Charger le contenu du fichier texte
-                $contenu = file_get_contents("./user_conditions.txt");
+                $contenu = file_get_contents($data["CONDITIONS_PATH"]);
 
-                // Insérer le contenu dans la balise <p>
                 echo "<p>$contenu</p>";
                 ?>
             </div>
@@ -428,32 +426,28 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
 
         <br>
         <label>
-            Je souhaite recevoir des informations de la part de “Au temps donné" par email
+            <?php echo htmlspecialchars($data["WISH_MAIL_NEWS"]);?>
             <input type="checkbox" id="email_info" name="conditions" value="email_info">
         </label>
-        <p>Les demandes seront examinées attentivement par notre équipe, qui se réserve le droit d'accepter ou de refuser
-            une demande en fonction des besoins de l'association et des disponibilités des bénévoles.</p>
+        <p><?php echo htmlspecialchars($data["DEMANDS_CHECKED"]);?>.</p>
 
-        <button id="validationButton" class="btn confirm-button">Valider</button>
+        <button id="validationButton" class="btn confirm-button"><?php echo htmlspecialchars($data["CONFIRM"]);?></button>
     </div> <!-- end of form-content -->
 </div> <!-- end of form-container -->
 
 <script>
     // Fonction pour valider les champs obligatoires avant d'envoyer les données à l'API
-    // Fonction pour valider le formulaire et envoyer les données à l'API
     function validateFormAndSendData() {
         // Récupérer les champs obligatoires
         var requiredFields = document.querySelectorAll('[required]');
 
-        // Parcourir les champs obligatoires
+
         for (var i = 0; i < requiredFields.length; i++) {
             var field = requiredFields[i];
 
-            // Vérifier si le champ est vide
             if (field.type === 'radio') {
                 var radioGroup = document.getElementsByName(field.name);
 
-                // Vérifier si aucun bouton radio n'est coché dans le groupe
                 var isChecked = false;
                 for (var j = 0; j < radioGroup.length; j++) {
                     if (radioGroup[j].checked) {
@@ -462,32 +456,24 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
                     }
                 }
 
-                // Si aucun bouton radio n'est coché, renvoyer vers les boutons radio
                 if (!isChecked) {
-                    // Faire défiler la page jusqu'au premier bouton radio du groupe
                     radioGroup[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-                    // Mettre en évidence tous les boutons radio du groupe
                     for (var k = 0; k < radioGroup.length; k++) {
                         radioGroup[k].style.outline = '2px solid red';
                     }
 
-                    // Arrêter la validation du formulaire
                     return false;
                 }
             } else if (field.value === '') {
-                // Faire défiler la page jusqu'au champ vide
                 field.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-                // Mettre en évidence le champ vide
                 field.style.border = '2px solid red';
 
-                // Arrêter la validation du formulaire
                 return false;
             }
         }
 
-        // Si tous les champs obligatoires sont remplis, envoyer les données à l'API
         sendDataToAPI();
     }
 
@@ -496,12 +482,10 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
         // Empêcher le comportement par défaut du bouton
         event.preventDefault();
 
-        // Appeler la fonction pour valider les champs et envoyer les données à l'API
         validateFormAndSendData();
     });
 
     function sendDataToAPI() {
-        // Récupérer les données du formulaire
         var genre = document.getElementById('genre').value;
         var nom = document.getElementById('nom').value;
         var prenom = document.getElementById('prenom').value;
@@ -516,7 +500,6 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
             permisArray.push(checkbox.value);
         });
 
-        // Créer un objet JSON avec les données du formulaire
         var data = {
             genre: genre,
             nom: nom,
@@ -530,10 +513,8 @@ echo "<title>Inscription bénévole - Au temps donné</title>";
         // Convertir l'objet JSON en chaîne JSON
         var jsonData = JSON.stringify(data);
 
-        // URL de votre API
         var apiUrl = 'http://localhost:8082/index.php/volunteers/register';
 
-        // Configuration de la requête POST
         var options = {
             method: 'POST',
             headers: {

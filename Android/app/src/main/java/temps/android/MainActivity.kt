f5adb1dc14acf59634e270611bd7e38a0fdc3d15
ShHaWkK <alexandreuzan9@gmail.com>
2024-05-12 03:17:49 +0200

    Update android

--- Android/app/src/main/java/temps/android/MainActivity.kt
+++ Android/app/src/main/java/temps/android/MainActivity.kt
@@ -1,5 +1,39 @@
 package temps.android
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import temps.android.ui.VolunteerScheduleFragment
 
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment) // Vous devez avoir un NavHostFragment dans votre layout
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home, R.id.nav_profile, R.id.nav_settings), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        // Supposons que vous voulez lancer une activité de connexion avant d'arriver ici
+        // Vous devriez alors initier MainActivity seulement après la connexion
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
--- Android/app/src/main/java/temps/android/ui/VolunteerScheduleFragment.kt
+++ Android/app/src/main/java/temps/android/ui/VolunteerScheduleFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,7 +24,23 @@ class VolunteerScheduleFragment : Fragment() {
 
     //------------------------------------------------------------------------------------------------------//
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            handleLogout()
+        }
+        view.findViewById<Button>(R.id.editProfileButton).setOnClickListener {
+            navigateToProfileEdit()
+        }
+    }
 
+    private fun handleLogout() {
+        // Implémenter la déconnexion
+    }
+
+    private fun navigateToProfileEdit() {
+        // Naviguer vers l'écran d'édition de profil
+    }
 
 
     override fun onCreateView(
